c627e84189c773737395552f4d05d288f3d27494
supercoolfire <u285916669@byui.edu>
2023-11-06 15:35:10 +0800

    just uploading

--- server.js
+++ server.js
@@ -5,7 +5,4 @@ const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-});
+app.listen(port, () => {console.log(`Running on port ${port}`);});
