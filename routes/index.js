43e3b9fbed391f98efb02f13f2f397f2984c3b3a
supercoolfire <u285916669@byui.edu>
2023-11-06 15:11:30 +0800

    remaining files upload

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => { 
+    res.send('Hello wolrds');
+});
+
+router.get('/frontend', (req, res) => { 
+    res.send('ello wolrds');
+});
+
+module.exports = router;
