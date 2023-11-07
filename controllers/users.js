4eaa276e0bbe7307f5de264c1f9b5065bc87af13
supercoolfire <u285916669@byui.edu>
2023-11-07 14:42:37 +0800

    Fix single user find

--- controllers/users.js
+++ controllers/users.js
@@ -10,12 +10,14 @@ const getAll = async (req, res) => {
 }
 
 const getSingle = async (req, res) => {
-  const userId = new Object(req.params.id);
-  const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId});
-  result.toArray().then((users) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDatabase().db().collection('users').findOne({ _id: userId });
+  if (result) {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(users[0]);
-  });
+    res.status(200).json(result);
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
 }
 
 module.exports = {
