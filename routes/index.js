620d4a90ee381fc3006412c68b2b8c18cc131d23
supercoolfire <u285916669@byui.edu>
2023-11-07 10:20:49 +0800

    frontend finaly works

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const express = require('express');
+const path = require('path');
 
 router.get('/', (req, res) => { res.send('Hello wolrds');});
 
+router.use('/frontend', express.static(path.join(__dirname, '../frontend')));
+
 router.get('/frontend', (req, res) => {
-  res.sendFile('../frontend/index.html');
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 module.exports = router;
