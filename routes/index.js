1beebf169780e684ec2fe91463ecea097ca60976
supercoolfire <u285916669@byui.edu>
2023-11-07 10:12:59 +0800

    Another one bites a dust

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
-const path = require('path');
 
 router.get('/', (req, res) => { res.send('Hello wolrds');});
 
-router.get('/frontend', (req, res) => { 
-  const filePath = path.join(__dirname, 'frontend', 'index.html');
-  res.sendFile(filePath);
+router.get('/frontend', (req, res) => {
+  res.sendFile('../frontend/index.html');
 });
 
 module.exports = router;
