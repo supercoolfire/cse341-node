4109f358220ee9a8266ff35510aad23d65066cc2
supercoolfire <u285916669@byui.edu>
2023-12-07 01:12:56 +0800

    invvitation is after visit

--- server.js
+++ server.js
@@ -83,7 +83,16 @@ app.get("/github/callback", passport.authenticate("github", {
 }),
   (req, res) => {
     req.session.user = req.user;
-    mongodb.getDatabase().db().collection('visitors').insertOne(req.session.user);
+    mongodb.getDatabase().db().collection('visitors').insertOne({ 
+      timestamp: new Date(), 
+      metadata: { 
+        user: req.user.username,
+        displayName: req.user.displayName,
+        profileUrl: req.user.profileUrl,
+        avatar_url: req.user.photos[0].value,
+        }
+    });
+    
     // console.log(req.user)
     res.redirect("/");
   });
