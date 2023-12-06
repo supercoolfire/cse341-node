c2dd9527ee05c96f006ad1444cf7ac34d033c559
supercoolfire <u285916669@byui.edu>
2023-12-06 19:44:49 +0800

    authenticate admin, moderator, default user

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const usersController = require('../controllers/users');
 const validation = require('../middleware/validate');
 const { isAuthenticated, isAdmin, isModerator } = require('../middleware/authenticate');
 
-router.get('/', isAdmin, isModerator, usersController.getAllUsers);
+router.get('/', isAuthenticated, usersController.getAllUsers);
 router.get('/:id', isAuthenticated, usersController.getSingleUser);
 router.post('/', isAuthenticated, validation.validateUser, usersController.createUser);
 router.put('/:id', isAuthenticated, validation.validateUser, usersController.updateUser);
