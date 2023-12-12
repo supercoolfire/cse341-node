0fd454350934b9aaac7159980495cece55611765
supercoolfire <u285916669@byui.edu>
2023-12-12 22:00:05 +0800

    authenication update

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const usersController = require('../controllers/users');
 const validation = require('../middleware/validate');
-const { isAuthenticated, isAdmin, isModerator } = require('../middleware/authenticate');
+const { isAuthenticated, isGod, isAdmin, isModerator } = require('../middleware/authenticate');
 
-router.get('/', isModerator, usersController.getAllUsers);
+router.get('/', isAuthenticated, usersController.getAllUsers);
 router.get('/:id', isAuthenticated, usersController.getSingleUser);
-router.post('/', isAdmin, validation.validateUser, usersController.createUser);
-router.put('/:id', isAuthenticated, validation.validateUser, usersController.updateUser);
-router.delete('/:id', isAdmin, usersController.deleteUser);
+router.post('/', isModerator, validation.validateUser, usersController.createUser);
+router.put('/:id', isAdmin, validation.validateUser, usersController.updateUser);
+router.delete('/:id', isGod, usersController.deleteUser);
 
 module.exports = router;
