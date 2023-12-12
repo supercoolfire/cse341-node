21af6ab559ce709730de79a546c51c1515b0f989
supercoolfire <u285916669@byui.edu>
2023-12-12 22:56:40 +0800

    fix browser autocomplete causing error by replacing guest with default

--- routes/override.js
+++ routes/override.js
@@ -2,27 +2,38 @@ const router = require('express').Router();
 const express = require('express');
 
 router.use((req, res, next) => {
-    // Check if the 'role' query parameter is present and has the value 'moderator'
-    if (req.query.role === 'admin' && !req.session.user) {
-        // Simulate a user with the role 'themoderator' and redirect
-        req.session.user = { username: 'theadmin' }
-        return res.redirect(req.originalUrl);
+    const queryParameter = req.query.role;
+    roles = ['god', 'admin', 'moderator', 'default'];
+    currentUser = req.session.user ? req.session.user.username !== undefined ? req.session.user.username : '' : '';
+    if (queryParameter) {
+        
+        // console.log('override req.session.user:', JSON.stringify(req.session.user, null, 2));
+        if (req.query.role === 'god' && (req.session.user === undefined || roles.includes(currentUser))) {
+            console.log('switching to god mode')
+            req.session.user = {}
+            req.session.user = { username: 'Thor', displayName: 'Thor' }
+            res.locals.message = `Logged in as ${req.session.user.username}`;
+            return res.redirect(req.originalUrl);
+        } else if (req.query.role === 'admin' && (req.session.user === undefined || roles.includes(req.session.user.username))) {
+            console.log('switching to admin mode')
+            req.session.user = {}
+            req.session.user = { username: 'theadmin', displayName: 'theadmin' }
+            return res.redirect(req.originalUrl);
+        } else if (req.query.role === 'moderator' && (req.session.user === undefined || roles.includes(req.session.user.username))) {
+            console.log('switching to moderator mode')
+            req.session.user = {}
+            req.session.user = { username: 'themoderator', displayName: 'themoderator' }
+            return res.redirect(req.originalUrl);
+        } else if (req.query.role === 'default' && (req.session.user === undefined || roles.includes(req.session.user.username))) {
+            console.log('switching to default mode')
+            req.session.user = {}
+            req.session.user = { username: 'Earthling', displayName: 'Earthling' }
+            return res.redirect(req.originalUrl);
+        }
+        console.log(`Role query parameter found: ${queryParameter}`)
+        // console.log(res.locals.message);
     }
-
-    if (req.query.role === 'moderator' && !req.session.user) {
-        // Simulate a user with the role 'themoderator' and redirect
-        req.session.user = { username: 'themoderator' }
-        return res.redirect(req.originalUrl);
-    }
-
-    if (req.query.role === 'guest' && !req.session.user) {
-        // Simulate a user with the role 'themoderator' and redirect
-        req.session.user = { username: 'Earthling' }
-        return res.redirect(req.originalUrl);
-    }
-
-    // Continue to the next middleware if not redirecting
     next();
 });
-
+ 
 module.exports = router;
