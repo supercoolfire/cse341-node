42c81d1a4f783aface4a1928f314ba5f3431805e
supercoolfire <u285916669@byui.edu>
2023-12-25 13:14:44 +0800

    modified access levvel

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const usersController = require('../controllers/users');
 const { validateUser } = require('../middleware/validate-users');
-const { isAuthenticated, isGod, isAdmin, isModerator } = require('../middleware/authenticate');
+const { accessLevel } = require('../middleware/authenticate-accessLevel');
 
-router.get('/', isAuthenticated, usersController.getAllUsers);
-router.get('/:id', isAuthenticated, usersController.getSingleUser);
-router.post('/', isAuthenticated, validateUser, usersController.createUser);
-router.put('/:id', isAuthenticated, validateUser, usersController.updateUser);
-router.delete('/:id', isAuthenticated, usersController.deleteUser);
+router.get('/', accessLevel(1), usersController.getAllUsers);
+router.get('/:id', accessLevel(1), usersController.getSingleUser);
+router.post('/', accessLevel(4), validateUser, usersController.createUser);
+router.put('/:id', accessLevel(4), validateUser, usersController.updateUser);
+router.delete('/:id', accessLevel(4), usersController.deleteUser);
 
 module.exports = router;
